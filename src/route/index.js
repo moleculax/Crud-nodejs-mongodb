a4e4ae870ea7fa2b80e2d206c057d9ff6116d268
Emilio Gomez <moleculax@gmail.com>
2019-01-01 12:13:06 -0400

    Vistas-Rutas-modelos

--- src/route/index.js
+++ src/route/index.js
@@ -21,4 +21,30 @@ router.post('/add',async (req,res)=>{
 	res.redirect('/'); //redirecciona a la raiz
 });
 
+router.get('/done/:id',async (req,res)=>{
+	const {id} = req.params;
+	const task = await Task.findById(id);
+	task.status = !task.status;
+	await task.save();
+	res.redirect('/');
+});
+
+router.get('/edit/:id', async (req, res, next) => {
+  const task = await Task.findById(req.params.id);
+  console.log(task)
+  res.render('edit', { task });
+});
+
+router.post('/edit/:id', async (req, res, next) => {
+  const { id } = req.params;
+  await Task.update({_id: id}, req.body);
+  res.redirect('/');
+});
+
+router.get('/delete/:id',async (req,res) => {
+	const {id} = req.params;
+	await Task.remove({_id: id});
+	res.redirect('/');
+});
+
 module.exports = router;
